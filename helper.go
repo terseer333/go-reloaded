48a37634baad6224352079bbb23165adb32c0e14
jakpera <terseerterese4@gmail.com>
2026-04-09 11:17:25 +0100

    go-reloaded

--- helper.go
+++ helper.go
@@ -76,16 +76,33 @@ func ToCap(str string) string {
 
 		}
 	}
-	return strings.Join(s, " ")
+	return strings.Join(s, " ")    //hdsd sdjd fd "(up,2)" ddsds
 }
 
 func LastTwoUpper(str string) string {
 	s := strings.Fields(str)
 	for i := 0; i < len(s); i++ {
-		if s[i] == "(up,2)" {
-			s[i-2] = strings.ToUpper(s[i-2])
-			s = append(s[:i], s[i+1:]...)
-			i++
+		if strings.HasPrefix(s[i], "(up,") {
+			var numba string
+			if strings.HasSuffix(s[i], ")") {
+				numba = strings.TrimPrefix(strings.TrimSuffix(s[i], ")"), "(up,")
+				s = append(s[:i], s[i+1:]...)
+				i--
+
+				count,err := strconv.Atoi(numba)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				start := i - count + 1
+				if start < 0 {
+					start = 0 
+				}
+
+				for j := start; j <=i; j++ {
+					s[j] = strings.ToUpper(s[j])
+				}
+			}
 
 		}
 	}
