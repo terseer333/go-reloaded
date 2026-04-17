83bdfc1ba4ce601efb5cb869e996feb407e22b97
Akpera james terese <terseerterese4@gmail.com>
2026-04-17 09:47:11 +0100

    punctuation.go

--- helper/punct.go
+++ helper/punct.go
@@ -4,13 +4,9 @@ import "regexp"
 
 func Punc(str string) string {
 
-	puc := regexp.MustCompile(`([a-zA-Z0-9])\s+([,.!:;?]+)([a-zA-Z0-9])`)
-	str = puc.ReplaceAllString(str, "$1$2 $3")
+	puc := regexp.MustCompile(`[ ]*([,.:;?!]+)`)
+	return puc.ReplaceAllString(str, `$1`)
 
-	puc2 := regexp.MustCompile(`\s+([,.:;?!])`)
-	str = puc2.ReplaceAllString(str, "$1")
+	
 
-	space := regexp.MustCompile(`\s{2,}(\s[a-zA-Z0-9])`)
-	str = space.ReplaceAllString(str, "$1")
-	return str
 }
